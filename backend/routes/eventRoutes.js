8595b17a92e2a30bc9dbe1a834e4a3bfc7dff353
deekshagarg <deekshagarg1405@gmail.com>
2026-04-27 09:56:28 +0530

    backend

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -1,32 +1,3 @@
-// const router = require("express").Router();
-// const event = require("../controllers/eventController");
-// const authMiddleware = require("../middleware/auth");  // ← naam change
-
-// // PUBLIC
-// router.get("/", event.getAllEvents);
-
-// // FACULTY
-// router.post("/create", authMiddleware, event.createEvent);
-// router.get("/my-events", authMiddleware, event.getMyEvents);
-// router.put("/update/:id", authMiddleware, event.updateEvent);
-// router.delete("/delete/:id", authMiddleware, event.deleteEvent);
-
-// // STUDENT
-// router.post("/register", authMiddleware, event.registerForEvent);
-// router.get("/my-registrations", authMiddleware, event.getMyRegistrations);
-
-// // ADMIN
-// router.get("/all-registrations", authMiddleware, event.getAllRegistrations);
-// router.get("/:id", event.getEventById);
-
-// const upload = require("../middleware/upload");
-
-// // Create route mein single image field
-// router.post("/create", authMiddleware, upload.single("image"), event.createEvent);
-
-// module.exports = router;
-
-
 
 const router = require("express").Router();
 const event = require("../controllers/eventController");
@@ -48,16 +19,4 @@ router.delete("/delete/:id", authMiddleware, event.deleteEvent);
 // STUDENT
 router.post("/register", authMiddleware, event.registerForEvent);
 
-// -------------------------------extra-------
-// const authMiddleware = require("../middleware/auth");
- 
-// // Profile fetch
-// router.get("/student/profile",  authMiddleware, auth.getStudentProfile);
-// router.get("/faculty/profile",  authMiddleware, auth.getFacultyProfile);
- 
-// // Profile update
-// router.put("/student/update",   authMiddleware, profileUpload.single("profile_pic"), auth.updateStudentProfile);
-// router.put("/faculty/update",   authMiddleware, profileUpload.single("profile_pic"), auth.updateFacultyProfile);
-
-// ---------------------
 module.exports = router;
